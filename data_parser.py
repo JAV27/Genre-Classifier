25ddb600997f5afd788224a4e2bc2fd3eacd7a8c
bill0259 <bill.ray0259@gmail.com>
2020-10-27 17:12:31 -0400

    Logistic regression

--- data_parser.py
+++ data_parser.py
@@ -1,27 +1,22 @@
 import pandas as pd
 
 artists = pd.read_csv("lyrics6genre/artists-data.csv", index_col="Link")
-# lyrics = pd.read_csv("lyrics6genre/lyrics-data.csv", index_col="ALink")
+lyrics = pd.read_csv("lyrics6genre/lyrics-data.csv", index_col="ALink")
 
-# data = pd.DataFrame()
+data = pd.DataFrame()
 
-# data = artists.join(lyrics)
+data = artists.join(lyrics)
 
-# data = data[data["Genre"] != "Sertanejo"]
-# data = data[data["Genre"] != "Funk Carioca"]
-# data = data[data["Genre"] != "Samba"]
-# data = data[data["Idiom"] == "ENGLISH"]
+data = data[data["Genre"] != "Sertanejo"]
+data = data[data["Genre"] != "Funk Carioca"]
+data = data[data["Genre"] != "Samba"]
+data = data[data["Idiom"] == "ENGLISH"]
 
-# data = data[["Genre", "Lyric"]]
-# data.index = range(len(data))
-# data.index.name = "index"
+data = data[["Genre", "Lyric"]]
+data.index = range(len(data))
+data.index.name = "index"
 
-# data.to_csv("data.csv")
+data.to_csv("data.csv")
 data = pd.read_csv("data.csv", index_col="index")
 
 print(data)
-
-# print(data)
-# print(data["Genre"] in genres)
-
-# print(data.loc[(data["Genre"] in genres).item()])
---
+++ logistic_regression.py
@@ -0,0 +1,63 @@
+from numpy.lib.scimath import log
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+df = pd.read_csv("data.csv", index_col="index")[:10000]
+df.columns = ["label", "text"]
+
+vectorizer = CountVectorizer(token_pattern=r"(?u)\b\w+'?\w*\b", stop_words="english")
+labels = df["label"].to_numpy()
+label_encoder = LabelEncoder()
+label_encoder.fit(["Rock", "Pop", "Hip Hop"])
+
+x = vectorizer.fit_transform(df["text"].to_numpy())
+y = label_encoder.transform(labels)
+
+train_x, test_x, train_y, test_y = train_test_split(x, y, test_size=0.4, random_state=42)
+
+logreg = LogisticRegression()
+logreg.fit(train_x, train_y)
+
+weights = logreg.coef_
+
+for i in range(3):
+    print("-------------")
+    maxVals = []
+    for j, data in enumerate(weights[i]):
+        maxVals.append((data, j))
+
+    sortedVals = sorted(maxVals, key=lambda x: x[0], reverse=True)
+
+    for k in sortedVals[:10]:
+        print(vectorizer.get_feature_names()[k[1]])
+
+preds = logreg.predict(test_x)
+print(accuracy_score(test_y, preds))
+print(confusion_matrix(test_y, preds))
+for i in range(3):
+    print(i)
+    print(precision_score(test_y, preds, labels=[i], average="weighted"))
+    print(recall_score(test_y, preds, labels=[i], average="weighted"))
+
+#Precision
+# if (matrix[0][0] + matrix[1][0] + matrix[2][0]) == 0:
+#     precisionEducation = 0
+# else:
+#     precisionEducation = matrix[0][0]/(matrix[0][0] + matrix[1][0] + matrix[2][0])
+
+# if (matrix[1][1] + matrix[0][1] + matrix[2][1]) == 0:
+# precisionFilm = 0
+# else:
+# precisionFilm = matrix[1][1]/(matrix[1][1] + matrix[0][1] + matrix[2][1])
+
+# if (matrix[1][1] + matrix[0][1] + matrix[2][1]) == 0:
+# precisionNeither = 0
+# else:
+# precisionNeither = matrix[2][2]/(matrix[2][2] + matrix[0][2] + matrix[1][2])
